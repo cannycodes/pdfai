cc4e9a0c24210fc0abb814483e54180369d324b2
Muhammad Arif <piaiclearning@gmail.com>
2023-10-04 19:51:13 +0500

    trying to resolve  edge function crash issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ export const metadata: Metadata = {
   title: 'AI Power',
   description: 'Harness The Power of AI to 10x your productivity',
 }
+export const runtime = 'edge' // 'nodejs' (default) | 'edge'
 
 export default function RootLayout({
   children,
